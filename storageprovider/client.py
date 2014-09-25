1ee2701a22a7cbeea6ed323cb71d1562196ba173
Bart Saelen <bart.saelen@geosolutions.be>
2014-09-25 10:13:08 +0200

    add oauth system token option for security

--- storageprovider/client.py
+++ storageprovider/client.py
@@ -9,78 +9,101 @@ class StorageProviderClient(object):
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def delete_object(self, container_key, object_key):
+    def delete_object(self, container_key, object_key, system_token=None):
         '''
         delete an object from the data store
 
         :param container_key: key of the container in the data store
         :param object_key: specific object key for the object in the container
+        :param system_token: oauth system token
         :raises InvalidStateException: if the response is in an invalid state
         '''
-        res = requests.delete(self.base_url + '/' + container_key + '/' + object_key)
+        headers = {}
+        if system_token:
+            headers = {'OpenAmSSOID': system_token}
+        res = requests.delete(self.base_url + '/' + container_key + '/' + object_key, headers=headers)
         if res.status_code != 200:
             raise InvalidStateException(res.status_code)
 
-    def get_object(self, container_key, object_key):
+    def get_object(self, container_key, object_key, system_token=None):
         '''
         retrieve an object from the data store
 
         :param container_key: key of the container in the data store
         :param object_key: specific object key for the object in the container
+        :param system_token: oauth system token
         :return content of the object
         :raises InvalidStateException: if the response is in an invalid state
         '''
-        res = requests.get(self.base_url + '/' + container_key + '/' + object_key)
+        headers = {}
+        if system_token:
+            headers = {'OpenAmSSOID': system_token}
+        res = requests.get(self.base_url + '/' + container_key + '/' + object_key, headers=headers)
         if res.status_code != 200:
             raise InvalidStateException(res.status_code)
         return res.content
 
-    def update_object(self, container_key, object_key, object_data):
+    def update_object(self, container_key, object_key, object_data, system_token=None):
         '''
         update (or create) an object in the data store
 
         :param container_key: key of the container in the data store
         :param object_key: specific object key for the object in the container
         :param object_data: data of the object
+        :param system_token: oauth system token
         :raises InvalidStateException: if the response is in an invalid state
         '''
         headers = {'content-type': 'application/octet-stream'}
+        if system_token:
+            headers['OpenAmSSOID'] = system_token
         res = requests.put(self.base_url + '/' + container_key + '/' + object_key, object_data, headers=headers)
         if res.status_code != 200:
             raise InvalidStateException(res.status_code)
 
-    def list_object_keys_for_container(self, container_key):
+    def list_object_keys_for_container(self, container_key, system_token=None):
         '''
         list all object keys for a container in the data store
 
         :param container_key: key of the container in the data store
+        :param system_token: oauth system token
         :return list of object keys found in the container
         :raises InvalidStateException: if the response is in an invalid state
         '''
-        res = requests.get(self.base_url + '/' + container_key)
+        headers = {}
+        if system_token:
+            headers = {'OpenAmSSOID': system_token}
+        res = requests.get(self.base_url + '/' + container_key, headers=headers)
         if res.status_code != 200:
             raise InvalidStateException(res.status_code)
         return res.content
 
-    def create_container(self, container_key):
+    def create_container(self, container_key, system_token=None):
         '''
         create a new container with specific key in the data store
 
         :param container_key: key of the container in the data store
+        :param system_token: oauth system token
         :raises InvalidStateException: if the response is in an invalid state
         '''
-        res = requests.put(self.base_url + '/' + container_key)
+        headers = {}
+        if system_token:
+            headers = {'OpenAmSSOID': system_token}
+        res = requests.put(self.base_url + '/' + container_key, headers=headers)
         if res.status_code != 200:
             raise InvalidStateException(res.status_code)
 
-    def create_container_and_key(self):
+    def create_container_and_key(self, system_token=None):
         '''
         create a new container in the data store and generate key
 
+        :param system_token: oauth system token
         :return the key generated for the container
         :raises InvalidStateException: if the response is in an invalid state
         '''
-        res = requests.post(self.base_url)
+        headers = {}
+        if system_token:
+            headers = {'OpenAmSSOID': system_token}
+        res = requests.post(self.base_url, headers=headers)
         if res.status_code != 201:
             raise InvalidStateException(res.status_code)
         container_key = res.json()['container_key']
@@ -88,14 +111,18 @@ class StorageProviderClient(object):
             container_key = str(container_key)
         return container_key
 
-    def delete_container(self, container_key):
+    def delete_container(self, container_key, system_token=None):
         '''
         delete a container in the data store
 
         :param container_key: key of the container in the data store
+        :param system_token: oauth system token
         :raises InvalidStateException: if the response is in an invalid state
         '''
-        res = requests.delete(self.base_url + '/' + container_key)
+        headers = {}
+        if system_token:
+            headers = {'OpenAmSSOID': system_token}
+        res = requests.delete(self.base_url + '/' + container_key, headers=headers)
         if res.status_code != 200:
             raise InvalidStateException(res.status_code)
 
--- tests/test_client.py
+++ tests/test_client.py
@@ -9,7 +9,6 @@ try:
 except ImportError:
     from mock import Mock, patch  # pragma: no cover
 
-
 test_container_key = 'test_container_key'
 test_object_key = 'test_object_key'
 test_base_url = 'http://localhost:6543/container'
@@ -28,7 +27,15 @@ class StorageProviderTest(unittest.TestCase):
     def test_delete_object(self, mock_requests):
         mock_requests.delete.return_value.status_code = 200
         self.storageproviderclient.delete_object(test_container_key, test_object_key)
-        mock_requests.delete.assert_called_with(test_base_url + '/' + test_container_key + '/' + test_object_key)
+        mock_requests.delete.assert_called_with(test_base_url + '/' + test_container_key + '/' + test_object_key
+                                                , headers={})
+
+    @patch('storageprovider.client.requests')
+    def test_delete_object_system_token(self, mock_requests):
+        mock_requests.delete.return_value.status_code = 200
+        self.storageproviderclient.delete_object(test_container_key, test_object_key, "x123-test")
+        mock_requests.delete.assert_called_with(test_base_url + '/' + test_container_key + '/' + test_object_key
+                                                , headers={"OpenAmSSOID": "x123-test"})
 
     @patch('storageprovider.client.requests')
     def test_delete_object_KO(self, mock_requests):
@@ -47,7 +54,15 @@ class StorageProviderTest(unittest.TestCase):
     def test_get_object(self, mock_requests):
         mock_requests.get.return_value.status_code = 200
         self.storageproviderclient.get_object(test_container_key, test_object_key)
-        mock_requests.get.assert_called_with(test_base_url + '/' + test_container_key + '/' + test_object_key)
+        mock_requests.get.assert_called_with(test_base_url + '/' + test_container_key + '/' + test_object_key
+                                             , headers={})
+
+    @patch('storageprovider.client.requests')
+    def test_get_object_system_token(self, mock_requests):
+        mock_requests.get.return_value.status_code = 200
+        self.storageproviderclient.get_object(test_container_key, test_object_key, "x123-test")
+        mock_requests.get.assert_called_with(test_base_url + '/' + test_container_key + '/' + test_object_key
+                                             , headers={"OpenAmSSOID": "x123-test"})
 
     @patch('storageprovider.client.requests')
     def test_get_object_KO(self, mock_requests):
@@ -74,6 +89,18 @@ class StorageProviderTest(unittest.TestCase):
                                              headers={'content-type': 'application/octet-stream'})
 
     @patch('storageprovider.client.requests')
+    def test_update_object_system_token(self, mock_requests):
+        kasteel = os.path.join(here, '..', 'fixtures/kasteel.jpg')
+        mock_requests.put.return_value.status_code = 200
+        bin_file = None
+        with open(kasteel, 'rb') as f:
+            bin_file = f.read()
+        self.storageproviderclient.update_object(test_container_key, test_object_key, bin_file, "x123-test")
+        mock_requests.put.assert_called_with(test_base_url + '/' + test_container_key + '/' + test_object_key, bin_file,
+                                             headers={"content-type": "application/octet-stream",
+                                                      "OpenAmSSOID": "x123-test"})
+
+    @patch('storageprovider.client.requests')
     def test_update_object_KO(self, mock_requests):
         kasteel = os.path.join(here, '..', 'fixtures/kasteel.jpg')
         error_thrown = False
@@ -92,7 +119,14 @@ class StorageProviderTest(unittest.TestCase):
     def test_list_object_keys_for_container(self, mock_requests):
         mock_requests.get.return_value.status_code = 200
         self.storageproviderclient.list_object_keys_for_container(test_container_key)
-        mock_requests.get.assert_called_with(test_base_url + '/' + test_container_key)
+        mock_requests.get.assert_called_with(test_base_url + '/' + test_container_key, headers={})
+
+    @patch('storageprovider.client.requests')
+    def test_list_object_keys_for_container_system_token(self, mock_requests):
+        mock_requests.get.return_value.status_code = 200
+        self.storageproviderclient.list_object_keys_for_container(test_container_key, "x123-test")
+        mock_requests.get.assert_called_with(test_base_url + '/' + test_container_key,
+                                             headers={"OpenAmSSOID": "x123-test"})
 
     @patch('storageprovider.client.requests')
     def test_list_object_keys_for_container_KO(self, mock_requests):
@@ -111,7 +145,14 @@ class StorageProviderTest(unittest.TestCase):
     def test_create_container(self, mock_requests):
         mock_requests.put.return_value.status_code = 200
         self.storageproviderclient.create_container(test_container_key)
-        mock_requests.put.assert_called_with(test_base_url + '/' + test_container_key)
+        mock_requests.put.assert_called_with(test_base_url + '/' + test_container_key, headers={})
+
+    @patch('storageprovider.client.requests')
+    def test_create_container_system_token(self, mock_requests):
+        mock_requests.put.return_value.status_code = 200
+        self.storageproviderclient.create_container(test_container_key, "x123-test")
+        mock_requests.put.assert_called_with(test_base_url + '/' + test_container_key,
+                                             headers={"OpenAmSSOID": "x123-test"})
 
     @patch('storageprovider.client.requests')
     def test_create_container_KO(self, mock_requests):
@@ -131,7 +172,15 @@ class StorageProviderTest(unittest.TestCase):
         mock_requests.post.return_value.status_code = 201
         mock_requests.post.return_value.json = Mock(return_value={'container_key': u'jk455'})
         res = self.storageproviderclient.create_container_and_key()
-        mock_requests.post.assert_called_with(test_base_url)
+        mock_requests.post.assert_called_with(test_base_url, headers={})
+        self.assertEqual('jk455', res)
+
+    @patch('storageprovider.client.requests')
+    def test_create_container_and_key_system_token(self, mock_requests):
+        mock_requests.post.return_value.status_code = 201
+        mock_requests.post.return_value.json = Mock(return_value={'container_key': u'jk455'})
+        res = self.storageproviderclient.create_container_and_key("x123-test")
+        mock_requests.post.assert_called_with(test_base_url, headers={"OpenAmSSOID": "x123-test"})
         self.assertEqual('jk455', res)
 
     @patch('storageprovider.client.requests')
@@ -139,7 +188,7 @@ class StorageProviderTest(unittest.TestCase):
         mock_requests.post.return_value.status_code = 201
         mock_requests.post.return_value.json = Mock(return_value={'container_key': 'jk455'})
         res = self.storageproviderclient.create_container_and_key()
-        mock_requests.post.assert_called_with(test_base_url)
+        mock_requests.post.assert_called_with(test_base_url, headers={})
         self.assertEqual('jk455', res)
 
     @patch('storageprovider.client.requests')
@@ -159,7 +208,14 @@ class StorageProviderTest(unittest.TestCase):
     def test_delete_container(self, mock_requests):
         mock_requests.delete.return_value.status_code = 200
         self.storageproviderclient.delete_container(test_container_key)
-        mock_requests.delete.assert_called_with(test_base_url + '/' + test_container_key)
+        mock_requests.delete.assert_called_with(test_base_url + '/' + test_container_key, headers={})
+
+    @patch('storageprovider.client.requests')
+    def test_delete_container_system_token(self, mock_requests):
+        mock_requests.delete.return_value.status_code = 200
+        self.storageproviderclient.delete_container(test_container_key, "x123-test")
+        mock_requests.delete.assert_called_with(test_base_url + '/' + test_container_key,
+                                                headers={"OpenAmSSOID": "x123-test"})
 
     @patch('storageprovider.client.requests')
     def test_delete_container_KO(self, mock_requests):
