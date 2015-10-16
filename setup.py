b0943cac0a53ae16bc1fedd8e1b6187712e669ef
Bart Saelen <bart.saelen@geosolutions.be>
2015-10-16 14:36:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
 ]
 
 setup(name='storageprovider-client',
-      version='1.1.0',
+      version='2.0.0',
       description='storageprovider client',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
